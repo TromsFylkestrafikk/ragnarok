eadaf5a30984c4996af802f138064a9e64df5be7
Kåre Slettnes <kaare.slettnes@tffk.no>
2023-03-06 13:50:15 +0100

    Syntax error fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
