e18b95b831ccab9abcd3510c937f22a233cfbc5e
Kåre Slettnes <kaare.slettnes@tffk.no>
2023-10-23 10:35:31 +0200

    tromsfylkestrafikk/ragnarok-consat#5: Set max age on CallDetails.csv

--- config/ragnarok_consat.php
+++ config/ragnarok_consat.php
@@ -1,12 +1,42 @@
 <?php
 
 return [
+    /*
+     |--------------------------------------------------------------------------
+     | Remote file system disk for Consat
+     |--------------------------------------------------------------------------
+     |
+     | This uses the same options as given in laravel's config/filesystems.php
+     |
+     */
+    'remote_disk' => [
+        'driver' => 'sftp',
+        'host' =>  env('CONSAT_HOST'),
+        'username' => env('CONSAT_USERNAME'),
+        'password' => env('CONSAT_PASSWORD'),
+        'port' => (int) env('CONSAT_PORT', 22),
+        'root' => env('CONSAT_ROOT', '/'),
+        'timeout' => 30,
+        'visibility' => 'public',
+        'directory_visibility' => 'public',
+    ],
 
     /*
      |--------------------------------------------------------------------------
-     | Disk name used for temporary files.
+     | Disk name used for temporary files
      |--------------------------------------------------------------------------
      */
     'tmp_disk' => 'local',
 
+    /*
+     |--------------------------------------------------------------------------
+     | Max age on imported csv files
+     |--------------------------------------------------------------------------
+     |
+     | Here you can specify max age of imported csv files. The age is given as a
+     | ISO-8601 duration.
+     */
+    'max_age' => [
+        'CallDetails.csv' => 'P30D',
+    ],
 ];
